4e7233402efd05e7ab54c000001c4ec2aa272e03
MarioHillenbrand <75442474+MarioHillenbrand@users.noreply.github.com>
2020-12-08 19:18:31 +0100

    Add files via upload

---
+++ Assets/Scripts/circle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+//Attach this script to a GameObject to rotate around the target position.
+public class circle : MonoBehaviour
+{
+    //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+    public int degree;
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        transform.RotateAround(target.transform.position, Vector3.up, degree * Time.deltaTime);
+    }
+}
